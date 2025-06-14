6a18ebff9df3722d103cd9ccf2dc9e0d4f949687
Raghupremshahapuram <100329614+Raghupremshahapuram@users.noreply.github.com>
2025-06-14 07:55:20 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,27 @@
 // db.js
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// module.exports = pool;
+
+// db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Render SSL
+  },
 });
 
 module.exports = pool;
+
